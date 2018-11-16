a7d0cd94720217ff913648ce028312d8a5452450
Jenna Pegg <jenna.pegg.vic@gmail.com>
2018-11-15 16:03:56 -0800

    add title and price to products, add gallery style to products

---
+++ themes/jenna-pegg-inhabitent/archive-product.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * The template for displaying archive pages.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+				?>
+
+			<div class="product-names">
+				<?php 
+				$terms = get_terms(array(
+					'taxonomy' => 'product_type',
+					'hide_empty' => 0,
+				));
+				?>
+				<?php foreach ($terms as $term):?>
+				<a href="<?php echo get_term_link($term); ?>"><?php echo $term->name;?></a>
+				<?php endforeach; ?>
+			</div>
+
+			</header><!-- .page-header -->
+
+			<section class="product-archive">
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+
+
+
+
+
+
+
+
+				
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php if ( has_post_thumbnail() ) : ?>
+			<a href="<?php echo get_the_permalink(); ?>"><?php the_post_thumbnail( 'large' ); ?></a>
+		<?php endif; ?>
+
+		<?php the_title(); ?>
+
+		<?php 
+		$price = '$' . CFS()->get( 'price' );
+
+		echo $price; 
+		?>
+
+		<?php if ( 'post' === get_post_type() ) : ?>
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+</article><!-- #post-## -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+			
+
+			<?php endwhile; ?>
+
+			<?php the_posts_navigation(); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
+</section>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
--- themes/jenna-pegg-inhabitent/single-product.php
+++ themes/jenna-pegg-inhabitent/single-product.php
@@ -22,7 +22,9 @@ get_header(); ?>
 
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
-        <?php echo CFS()->get( 'price' ); ?>
+		<?php echo CFS()->get( 'price' ); 
+		?>
+		
 
 		<div class="entry-meta">
 			<?php red_starter_posted_on(); ?> / <?php red_starter_comment_count(); ?> / <?php red_starter_posted_by(); ?>
