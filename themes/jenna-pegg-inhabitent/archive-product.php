54815dd72e98ac69dab7b4799951743656aff72b
Jenna Pegg <jenna.pegg.vic@gmail.com>
2018-11-15 16:50:33 -0800

    finish product page

--- themes/jenna-pegg-inhabitent/archive-product.php
+++ themes/jenna-pegg-inhabitent/archive-product.php
@@ -35,50 +35,27 @@ get_header(); ?>
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-
-
-
-
-
-
-
-
-
 				
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php if ( has_post_thumbnail() ) : ?>
-			<a href="<?php echo get_the_permalink(); ?>"><?php the_post_thumbnail( 'large' ); ?></a>
-		<?php endif; ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="entry-header">
+						<?php if ( has_post_thumbnail() ) : ?>
+							<a href="<?php echo get_the_permalink(); ?>"><?php the_post_thumbnail( 'large' ); ?></a>
+						<?php endif; ?>
 
-		<?php the_title(); ?>
+						<div class="product-info">
+							<p class="product-name"><?php the_title(); ?></p>
+							<div class="dots"></div>
+							<p class="product-price"><?php 
+							$price = '$' . CFS()->get( 'price' );
 
-		<?php 
-		$price = '$' . CFS()->get( 'price' );
+							echo $price; 
+							?></p>
+						</div>
+						<?php if ( 'post' === get_post_type() ) : ?>
+						<?php endif; ?>
+					</header><!-- .entry-header -->
 
-		echo $price; 
-		?>
-
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<?php endif; ?>
-	</header><!-- .entry-header -->
-
-</article><!-- #post-## -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-			
-
+				</article><!-- #post-## -->
 			<?php endwhile; ?>
 
 			<?php the_posts_navigation(); ?>
