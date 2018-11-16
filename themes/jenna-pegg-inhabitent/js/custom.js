729552d4ffb27921ebec56348bda69494fd19f1b
Jenna Pegg <jenna.pegg.vic@gmail.com>
2018-11-16 12:07:18 -0800

    add searchbar, and toggle it in js

--- themes/jenna-pegg-inhabitent/js/custom.js
+++ themes/jenna-pegg-inhabitent/js/custom.js
@@ -1,6 +1,22 @@
 // immediately invoked function expression
 (function($) {
   $(document).ready(function() {
-    console.log('hello');
-  });
+    const $submit = '.main-navigation .icon-search';
+    const $field = '.main-navigation .search-field';
+
+    $($submit).on('click', function() {
+      event.preventDefault();
+      if (this) {
+        $($field)
+          .toggle()
+          .focus();
+      }
+    }); //end of on click
+
+    $($field).blur(function() {
+      if (this) {
+        $($field).hide();
+      }
+    }); //end of blur function
+  }); //end of doc ready
 })(jQuery);
