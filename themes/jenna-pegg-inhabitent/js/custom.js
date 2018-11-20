68309282f5b11e29cf656e8e2842776b135e42fe
Jenna Pegg <jenna.pegg.vic@gmail.com>
2018-11-19 16:26:21 -0800

    add scroll color change

--- themes/jenna-pegg-inhabitent/js/custom.js
+++ themes/jenna-pegg-inhabitent/js/custom.js
@@ -1,12 +1,16 @@
 // immediately invoked function expression
 (function($) {
   $(document).ready(function() {
+
+    $('nav').addClass('colors');
+
     const $submit = $('.main-navigation .icon-search');
     const $field = $('.main-navigation .search-field');
+    let $navigation = $('.main-navigation');
 
     $submit.on('click', function() {
       // event.preventDefault();
-      $field.toggle();
+      $field.toggle(1000);
       $field.focus();
     }); //end of on click
 
@@ -16,5 +20,12 @@
         $field.hide();
       }
     }); //end of blur function
+    $(window).scroll(function() {
+      if ($(document).scrollTop() > 600) {
+        $('nav').removeClass('colors');
+      } else {
+        $('nav').addClass('colors');
+      }
+    }); //end of scroll
   }); //end of doc ready
 })(jQuery);
