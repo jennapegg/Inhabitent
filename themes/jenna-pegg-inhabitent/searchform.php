9d3a34330e47933e6138ad51c6f689dce20b61e6
Jenna Pegg <jenna.pegg.vic@gmail.com>
2018-11-27 09:51:17 -0800

    move search icon to otherside

--- themes/jenna-pegg-inhabitent/searchform.php
+++ themes/jenna-pegg-inhabitent/searchform.php
@@ -1,11 +1,10 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-		<label>
-			<input type="search" class="search-field" placeholder="Type and hit enter ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
-		</label>
 		<span class="icon-search" aria-hidden="true">
 				<i class="fa fa-search"></i>
 		</span>
-
+		<label>
+			<input type="search" class="search-field" placeholder="Type and hit enter ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
+		</label>
 		<button class="search-submit">
 			<span class="screen-reader-text"><?php echo esc_html( 'Type and hit enter' ); ?></span>
 		</button>
